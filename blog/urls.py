f30a5896deb844abba56d71b78c1b903d1e41f22
dapoadedire <adedireadedapo19@gmail.com>
2022-07-02 09:33:39 +0100

    feat: add about and contact views, urls and template

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path("delete/<slug:slug>", views.PostDeleteView.as_view(), name="delete"),
     path("update/<slug:slug>", views.PostUpdateView.as_view(), name="update"),
     path("post/<slug:slug>", views.PostDetailView.as_view(), name="detail"),
+    path("about/", views.AboutView.as_view(), name="about"),
+    path("contact/", views.ContactView.as_view(), name="contact"),
 
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,19 @@
+from re import template
 from django.shortcuts import render
-
+from django.views.generic import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 from django.urls import reverse_lazy
 from blog.models import Post
+
 # Create your views here.
+
 
 class PostListView(ListView):
     model = Post
+
     def get_queryset(self):
         return super().get_queryset().filter(status="published")
 
@@ -18,18 +22,25 @@ class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:home")
-  
+
+
 class PostDetailView(DetailView):
     model = Post
-   
+
+
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
     success_url = reverse_lazy("blog:home")
-   
 
 
 class PostDeleteView(DeleteView):
     model = Post
     success_url = reverse_lazy("blog:home")
-   
+
+
+class AboutView(View):
+    template_name = "blog/about.html"
+
+class ContactView(View):
+    template_name = "blog/contact.html"
