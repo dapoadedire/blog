9029bf15f081a04f50e64833a75f54e26ed67347
dapoadedire <adedireadedapo19@gmail.com>
2022-07-02 08:57:15 +0100

    perf: change url to CBV URLS

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
+
+app_name = "blog"
+
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
-    path('contact/', views.contact, name='blog-contact'),
-    path('detail/', views.blog_detail, name='blog-detail'),
-    # path('blog/<int:blog_id>/', views.blog_detail, name='blog-detail'),
+    path("", views.PostListView.as_view(), name="home"),
+    path("create/", views.PostCreateView.as_view(), name="create"),
+    path("delete/<slug:slug>", views.PostDeleteView.as_view(), name="delete"),
+    path("update/<slug:slug>", views.PostUpdateView.as_view(), name="update"),
+    path("post/<slug:slug>", views.PostDetailView.as_view(), name="detail"),
 
 ]
