b75ea17e06a20b70ada6784ae5568b575028a35c
dapoadedire <adedireadedapo19@gmail.com>
2022-07-03 09:32:58 +0100

    feat: add custom user

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,8 @@ from django.template.defaultfilters import slugify
 from django.urls import reverse
 from django.utils import timezone
 from taggit.managers import TaggableManager
-
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
 # Create your models here.
 
 
@@ -14,7 +15,7 @@ class Post(models.Model):
     content = RichTextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     date_updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     date_published = models.DateTimeField(default=timezone.now)
     image = models.ImageField(
         upload_to="images/", default="images/default.jpg", blank=True
